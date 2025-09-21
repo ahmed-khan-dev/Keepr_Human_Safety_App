18d239468b8007d3796697cba4c0cc409bf53d70
ahmed-khan-dev <ahmedfk9214@gmail.com>
2025-09-21 18:09:14 +0530

    login activity

---
+++ app/src/main/java/com/example/keepr_humansafetyapp/LoginActivity.kt
@@ -0,0 +1,47 @@
+package com.example.keepr_humansafetyapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            // User is already logged in → Go directly to MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            // No user logged in → Show login screen
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.login_container, LoginFragment.newInstance())
+                    .commit()
+            }
+        }
+    }
+
+
+    // Helper function to open MainActivity after login success
+    fun openMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()  // So that user cannot go back to LoginActivity
+    }
+
+    fun openRegisterFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.login_container, RegisterFragment.newInstance())
+            .addToBackStack(null) // This allows the back button to go back to LoginFragment
+            .commit()
+    }
+
+
+}
